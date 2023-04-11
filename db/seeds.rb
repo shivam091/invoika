bfcbbce9ca6d59f40627abff15ff5ae3bc7b97a8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-11 18:39:43 +0530

    Added seeds for `roles`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 # -*- encoding: utf-8 -*-
 # -*- frozen_string_literal: true -*-
 # -*- warn_indent: true -*-
+
+["admin", "client"].each do |role_name|
+  ::Role.safe_find_or_create_by(name: role_name)
+end
