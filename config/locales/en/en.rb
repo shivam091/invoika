c2a0f35787feaa2850a762f270bdd1d07d38355e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-11 11:23:29 +0530

    Added application wide translations

--- config/locales/en/en.rb
+++ config/locales/en/en.rb
@@ -1,33 +1,32 @@
-# Files in the config/locales directory are used for internationalization
-# and are automatically loaded by Rails. If you want to use locales other
-# than English, add the necessary files in this directory.
-#
-# To use the locales, use `I18n.t`:
-#
-#     I18n.t "hello"
-#
-# In views, this is aliased to just `t`:
-#
-#     <%= t("hello") %>
-#
-# To use a different locale, set it with `I18n.locale`:
-#
-#     I18n.locale = :es
-#
-# This would use the information in config/locales/es.yml.
-#
-# The following keys must be escaped otherwise they will not be retrieved by
-# the default I18n backend:
-#
-# true, false, on, off, yes, no
-#
-# Instead, surround them with single quotes.
-#
-# en:
-#   "true": "foo"
-#
-# To learn more, please read the Rails Internationalization guide
-# available at https://guides.rubyonrails.org/i18n.html.
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
 
-en:
-  hello: "Hello world"
+{
+  en: {
+    direction: "ltr",
+    boolean: {
+      "yes": "Yes",
+      "no": "No",
+      nil: "Nothing",
+    },
+    pagy: {
+      item_name: {
+        one: "item",
+        other: "items",
+      },
+      nav: {
+        prev: "&laquo;&nbsp;Previous",
+        next: "Next&nbsp;&raquo;",
+        gap: "&hellip;"
+      },
+      info: {
+        no_items: "No %{item_name} found",
+        single_page: "Displaying <strong>%{count}</strong> %{item_name}",
+        multiple_pages: "Displaying %{item_name} <strong>%{from} - %{to}</strong> of <strong>%{count}</strong> in total",
+      },
+      combo_nav_js: "Page %{page_input} of %{pages}",
+      items_selector_js: "Show %{items_input} %{item_name} per page"
+    },
+  }
+}
