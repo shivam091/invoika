521b24d344dd30864e83414ed055c6a64353374a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-11 22:31:01 +0530

    Overridden `root_path` to resolve role wise home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  helper_method def root_path
+    case
+    when user_signed_in?
+      case
+      when current_user.admin? then admin_dashboard_path
+      when current_user.client? then client_dashboard_path
+      end
+    else new_user_session_path
+    end
+  end
+
   def render_flash
     turbo_stream.update(:flash, partial: "shared/flash_messages")
   end
