138c351ec12b000c54dfc02eaf379b5c0c3ccb7c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-16 20:02:05 +0530

    Added `validations` and `modules` in `Category`

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,24 @@
 # -*- warn_indent: true -*-
 
 class Category < ApplicationRecord
+  include Sortable, Filterable, Toggleable
+
+  attribute :is_active, default: 0
+  attribute :products_count, default: 0
+
+  validates :name,
+            presence: true,
+            uniqueness: {scope: :user_id},
+            length: {maximum: 55},
+            reduce: true
+  validates :user_id, presence: true, reduce: true
+  validates :products_count,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than: 0
+            },
+            reduce: true
+
   belongs_to :user, inverse_of: :categories
 end
