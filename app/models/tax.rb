b56dd8f60970b655f949fb279ba5258072e68298
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-16 19:44:10 +0530

    Added required `validations` in `Tax` model

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -5,5 +5,28 @@
 class Tax < ApplicationRecord
   self.inheritance_column = :_type_disabled
 
+  enum type: {
+    inclusive: "inclusive",
+    exclusive: "exclusive"
+  }
+
+  attribute :type, :enum, default: types[:exclusive]
+  attribute :is_active, default: false
+
+  validates :name,
+            presence: true,
+            uniqueness: {scope: :user_id},
+            length: {maximum: 55},
+            reduce: true
+  validates :user_id, presence: true, reduce: true
+  validates :rate,
+            presence: true,
+            numericality: {greater_than: 0.0},
+            reduce: true
+  validates :type,
+            presence: true,
+            inclusion: {in: types.values},
+            reduce: true
+
   belongs_to :user, inverse_of: :taxes
 end
