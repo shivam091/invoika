64b1e254ae1e0300990aff5e95077100731bc85e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 10:29:39 +0530

    Added controller and routes for `categories`

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,23 @@ Rails.application.routes.draw do
     resource :profile, only: [:show, :edit, :update]
   end
 
+  concern :toggleable do
+    collection do
+      get :active
+      get :inactive
+    end
+
+    member do
+      patch :activate
+      patch :deactivate
+    end
+  end
+
   authenticated :user do
     namespace :admin do
       concerns :shareable
+
+      resources :categories, except: :show, concerns: :toggleable
     end
 
     namespace :client do
