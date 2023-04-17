516f74b9fa6afaf11a19ca813e6606ee1fcf8aed
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 19:29:47 +0530

    Added feature to `create` the `product`

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -21,4 +21,43 @@ class Admin::ProductsController < Admin::BaseController
     @products = current_user.products.inactive
     @pagy, @products = pagy(@products)
   end
+
+  # GET /admin/products/new
+  def new
+    @product = current_user.products.build
+  end
+
+  # POST /admin/products
+  def create
+    response = ::Products::CreateService.(current_user, product_params)
+    @product = response.payload[:product]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_products_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:product_form, partial: "admin/products/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :code,
+      :description,
+      :unit_price,
+      :sell_price,
+      :category_id,
+      :is_active
+    )
+  end
 end
