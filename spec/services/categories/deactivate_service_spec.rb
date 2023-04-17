ce4a9add2efca24618a85d310121a5894b14ef59
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 11:25:25 +0530

    Added service to `deactivate` the `category`

---
+++ spec/services/categories/deactivate_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/categories/deactivate_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe Categories::DeactivateService, type: :service do
+end
