003ec38c136f54aa6596b08657e36d80e1b63e12
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 11:16:09 +0530

    Added feature to `create` the `category`

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -21,4 +21,35 @@ class Admin::CategoriesController < Admin::BaseController
     @categories = current_user.categories.inactive
     @pagy, @categories = pagy(@categories)
   end
+
+  # GET /admin/categories/new
+  def new
+    @category = current_user.categories.build
+  end
+
+  # POST /admin/categories
+  def create
+    response = ::Categories::CreateService.(current_user, category_params)
+    @category = response.payload[:category]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_categories_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:category_form, partial: "admin/categories/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :is_active)
+  end
 end
