92c9dd598644f359cfc059bf732f2e41c8391945
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 12:54:44 +0530

    Added feature to `create` the `tax`

--- app/controllers/admin/taxes_controller.rb
+++ app/controllers/admin/taxes_controller.rb
@@ -21,4 +21,35 @@ class Admin::TaxesController < Admin::BaseController
     @taxes = current_user.taxes.inactive
     @pagy, @taxes = pagy(@taxes)
   end
+
+  # GET /admin/taxes/new
+  def new
+    @tax = current_user.taxes.build
+  end
+
+  # POST /admin/taxes
+  def create
+    response = ::Taxes::CreateService.(current_user, tax_params)
+    @tax = response.payload[:tax]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_taxes_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:tax_form, partial: "admin/taxes/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def tax_params
+    params.require(:tax).permit(:name, :rate, :type, :is_active)
+  end
 end
