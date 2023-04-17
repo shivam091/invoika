8e46cc533f006964aeaddb5340a158992f2d2d1a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 19:33:14 +0530

    Added feature to `update` the `product`

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,6 +4,8 @@
 
 class Admin::ProductsController < Admin::BaseController
 
+  before_action :find_product, except: [:index, :active, :inactive, :new, :create]
+
   # GET /admin/products
   def index
     @products = current_user.products
@@ -47,7 +49,35 @@ class Admin::ProductsController < Admin::BaseController
     end
   end
 
+  # GET /admin/products/:uuid/edit
+  def edit
+  end
+
+  # PUT/PATCH /admin/products/:uuid
+  def update
+    response = ::Products::UpdateService.(@product, product_params)
+    @product = response.payload[:product]
+    if response.success?
+      flash[:notice] = response.message
+      redirect_to admin_products_path
+    else
+      flash.now[:alert] = response.message
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(:product_form, partial: "admin/products/form"),
+            render_flash
+          ], status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
   private
+
+  def find_product
+    @product = current_user.products.find(params.fetch(:uuid))
+  end
 
   def product_params
     params.require(:product).permit(
