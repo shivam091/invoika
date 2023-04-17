873a7b54d5e87b8b3bebe21899d832a1531361c6
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-17 18:07:29 +0530

    Added feature to list active the `products`

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,4 +9,10 @@ class Admin::ProductsController < Admin::BaseController
     @products = current_user.products
     @pagy, @products = pagy(@products)
   end
+
+  # GET /admin/products/active
+  def active
+    @products = current_user.products.active
+    @pagy, @products = pagy(@products)
+  end
 end
