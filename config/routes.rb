e6262ee5f4e4abaadff330ae26e9cb44f123e3dd
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-19 19:43:48 +0530

    Added controller and routes for `quotes` section

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
   concern :shareable do
     resource :dashboard, only: :show
     resource :profile, only: [:show, :edit, :update]
+    resources :quotes
   end
 
   concern :toggleable do
