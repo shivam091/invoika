06740f54280d9fe752efbe8141a01134e7a05f5c
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-19 14:21:13 +0530

    Added enum `discount_types`

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_083626) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_083637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "discount_types", [["flat", "percentage"]]
   create_enum "quote_statuses", [["draft", "converted", "pending", "accepted", "not_accepted"]]
   create_enum "tax_types", [["inclusive", "exclusive"]]
 
