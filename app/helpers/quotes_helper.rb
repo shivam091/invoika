7b218e3b282e62dc706effc72ccfd79fbd36374d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-20 13:06:01 +0530

    Added helper methods to resolve routes of `quotes` section

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -3,4 +3,59 @@
 # -*- warn_indent: true -*-
 
 module QuotesHelper
+  def quotes_path
+    case
+    when current_user.admin? then admin_quotes_path
+    when current_user.client? then client_quotes_path
+    end
+  end
+
+  def draft_quotes_path
+    case
+    when current_user.admin? then draft_admin_quotes_path
+    when current_user.client? then draft_client_quotes_path
+    end
+  end
+
+  def converted_quotes_path
+    case
+    when current_user.admin? then converted_admin_quotes_path
+    when current_user.client? then converted_client_quotes_path
+    end
+  end
+
+  def accepted_quotes_path
+    case
+    when current_user.admin? then accepted_admin_quotes_path
+    when current_user.client? then accepted_client_quotes_path
+    end
+  end
+
+  def new_quote_path
+    case
+    when current_user.admin? then new_admin_quote_path
+    when current_user.client? then new_client_quote_path
+    end
+  end
+
+  def edit_quote_path(quote)
+    case
+    when current_user.admin? then edit_admin_quote_path(quote)
+    when current_user.client? then edit_client_quote_path(quote)
+    end
+  end
+
+  def quote_path(quote)
+    case
+    when current_user.admin? then admin_quote_path(quote)
+    when current_user.client? then client_quote_path(quote)
+    end
+  end
+
+  def quote_object(quote)
+    case
+    when current_user.admin? then [:admin, quote]
+    when current_user.client? then [:client, quote]
+    end
+  end
 end
