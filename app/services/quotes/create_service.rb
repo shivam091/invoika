2568f1e40e768c34c0beaaebb75cb36dd173b905
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-20 12:14:48 +0530

    Added service to `create` the `quote`

---
+++ app/services/quotes/create_service.rb
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class Quotes::CreateService < ApplicationService
+  def initialize(user, quote_attributes)
+    @user = user
+    @quote_attributes = quote_attributes.dup
+  end
+
+  def call
+    create_quote
+  end
+
+  private
+
+  attr_reader :user, :quote_attributes
+
+  def create_quote
+    quote = user.created_quotes.build(quote_attributes)
+    if quote.save
+      ::ServiceResponse.success(
+        message: t("quotes.create.success"),
+        payload: {quote: quote}
+      )
+    else
+      ::ServiceResponse.error(
+        message: t("categories.create.error"),
+        payload: {quote: quote}
+      )
+    end
+  end
+end
