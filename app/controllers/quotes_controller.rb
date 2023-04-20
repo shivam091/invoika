8a2f555f717c3042199c58c69cf19321eb2e7434
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-20 12:58:44 +0530

    Added feature to list all `quotes`

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,10 @@
 # -*- warn_indent: true -*-
 
 class QuotesController < ApplicationController
+
+  # GET /(:role)/quotes
+  def index
+    @quotes = ::Quote.accessible(current_user)
+    @pagy, @quotes = pagy(@quotes)
+  end
 end
