ae1ede7fa66fcfecd6d5369ff3b5214efdbb5e47
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-21 10:40:38 +0530

    Added required `methods`, `validations`, and `default scope` in `Invoice` model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,86 @@
 
 class Invoice < ApplicationRecord
 
+  include Sortable
+
+  enum discount_type: {
+    flat: "flat",
+    percentage: "percentage"
+  }
+
+  enum status: {
+    draft: "draft",
+    unpaid: "unpaid",
+    paid: "paid",
+    partially_paid: "partially_paid",
+    processing: "processing",
+    overdue: "overdue",
+    void: "void",
+    uncollectible: "uncollectible"
+  }
+
+  attribute :status, :enum, default: statuses[:draft]
+  attribute :discount_type, :enum, default: discount_types[:flat]
+
+  validates :client_id, :user_id, presence: true, reduce: true
+  validates :code,
+            presence: true,
+            uniqueness: {scope: :user_id},
+            length: {maximum: 15},
+            reduce: true
+  validates :invoice_date, presence: true, reduce: true
+  validates :due_date,
+            presence: true,
+            comparison: {greater_than_or_equal_to: :invoice_date},
+            reduce: true
+  validates :status,
+            presence: true,
+            inclusion: {in: statuses.values},
+            reduce: true
+  validates :discount_type,
+            inclusion: {in: discount_types.values},
+            allow_nil: true,
+            reduce: true
+  validates :discount, presence: true, if: :discount_required?, reduce: true
+  validates :discount_type, presence: true, if: :discount_type_required?, reduce: true
+  validates :notes, :terms,
+            length: {maximum: 1000},
+            allow_nil: true,
+            allow_blank: true,
+            reduce: true
+  validates :recurring_cycle,
+            presence: true,
+            reduce: true,
+            if: :is_recurred?
+
   belongs_to :client, class_name: "::User", inverse_of: :invoices
   belongs_to :user, inverse_of: :created_invoices
+
+  after_initialize :set_code, if: :new_record?
+
+  delegate :full_name, to: :client, prefix: true
+  delegate :full_name, to: :user, prefix: true
+
+  default_scope -> { order_created_desc }
+
+  class << self
+    def accessible(user)
+      return where(client_id: user.id) if user.client?
+      all
+    end
+  end
+
+  private
+
+  def set_code
+    self.code = SecureRandom.alphanumeric(8).upcase
+  end
+
+  def discount_required?
+    discount_type.present?
+  end
+
+  def discount_type_required?
+    discount.present?
+  end
 end
