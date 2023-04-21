3ed95e4b292a6255cf69a2fc2f97fd86c2993738
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-21 12:09:24 +0530

    Added required `validations` in `InvoiceItem` model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,6 +3,19 @@
 # -*- warn_indent: true -*-
 
 class InvoiceItem < ApplicationRecord
+  attribute :quantity, default: 1
+  attribute :total_amount, default: 0
+
+  validates :product_id, presence: true, reduce: true
+  validates :quantity,
+            presence: true,
+            numericality: {only_integer: true, greater_than_or_equal_to: 1},
+            reduce: true
+  validates :unit_price,
+            presence: true,
+            numericality: true,
+            reduce: true
+
   belongs_to :invoice, inverse_of: :invoice_items
   belongs_to :product, inverse_of: :invoice_items
 end
