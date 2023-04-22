a3a15bcdf9573360d2cbc0ca603cc1f77a2dde9e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-22 21:52:24 +0530

    Added service to `update` the `user preferences`

---
+++ app/services/user_preferences/update_service.rb
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class UserPreferences::UpdateService < ApplicationService
+  def initialize(user, preference_attributes)
+    @user = user
+    @preference_attributes = preference_attributes.dup
+  end
+
+  def call
+    update_preferences
+  end
+
+  private
+
+  attr_reader :user, :preference_attributes
+
+  def update_preferences
+    if user.update(preference_attributes)
+      ::ServiceResponse.success(
+        message: t("preferences.update.success"),
+        payload: {user: user}
+      )
+    else
+      ::ServiceResponse.error(
+        message: t("preferences.update.error"),
+        payload: {user: user}
+      )
+    end
+  end
+end
