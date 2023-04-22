3b0765875e3cd682d8a85fbee3f8d7e9ccf7b02f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-22 11:09:00 +0530

    Changes in `recurring_cycle_field_controller`

--- app/javascript/controllers/recurring_cycle_field_controller.js
+++ app/javascript/controllers/recurring_cycle_field_controller.js
@@ -2,18 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
 
+  static targets = ["isRecurred", "recurringCycle", "recurringCycleWrapper"]
+
   connect() {
     this.toggleRecurringCycle();
   }
 
   toggleRecurringCycle(event) {
     event?.preventDefault();
-    if (this.element.checked) {
-      document.querySelector("#invoice_recurring_cycle").removeAttributes("readonly", "disabled");
-      document.querySelector("#recurring_cycle_period").style.display = "block";
+    if (this.isRecurredTarget.checked) {
+      this.recurringCycleTarget.removeAttributes("readonly", "disabled");
+      this.recurringCycleWrapperTarget.style.display = "block";
+      this.recurringCycleTarget.focus();
     } else {
-      document.querySelector("#invoice_recurring_cycle").setAttributes({"readonly": "readonly", "disabled": "disabled"});
-      document.querySelector("#recurring_cycle_period").style.display = "none";
+      this.recurringCycleWrapperTarget.style.display = "none";
+      this.recurringCycleTarget.value = "";
+      this.recurringCycleTarget.setAttributes({"readonly": "readonly", "disabled": "disabled"});
     }
   }
 }
