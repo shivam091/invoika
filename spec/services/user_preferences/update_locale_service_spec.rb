33e37861b194fe9257740a3418dc3e824470b6a0
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-04-24 09:50:50 +0530

    Added service to `update` user `locale`

---
+++ spec/services/user_preferences/update_locale_service_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/services/user_preferences/update_locale_service_spec.rb
+
+require "spec_helper"
+
+RSpec.describe UserPreferences::UpdateLocaleService, type: :service do
+end
