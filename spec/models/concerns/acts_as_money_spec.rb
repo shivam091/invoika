bf185c1a80268da612cd7c65725483f7b7efdf2b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-01 11:41:52 +0530

    Added concern `ActsAsMoney` for dealing with `money` and `currency`

---
+++ spec/models/concerns/acts_as_money_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/models/concerns/acts_as_money_spec.rb
+
+require "spec_helper"
+
+RSpec.describe ActsAsMoney do
+end
