126a1360533081564da1aedc659278018a7d6f4f
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-06 18:28:15 +0530

    Added `Ability` for `taxes`

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    if user.client?
+
+    end
+
+    if user.vendor?
+      can :manage, ::Tax
+    end
+  end
+end
