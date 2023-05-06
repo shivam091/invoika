6dbd929c07b8d1d2de6f05e19a57da1a1f2dfaff
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-06 13:11:19 +0530

    Added translations

--- config/locales/en/admin/dashboards.rb
+++ config/locales/en/admin/dashboards.rb
@@ -7,7 +7,24 @@
     admin: {
       dashboards: {
         show: {
-          title: "Dashboard"
+          title: "Dashboard",
+          total_clients: "Total clients",
+          total_products: "Total products",
+          total_quotes: "Total quotes",
+          total_invoices: "Total invoices",
+          total_paid_invoices: "Total paid invoices",
+          total_unpaid_invoices: "Total unpaid invoices",
+          total_overdue_invoices: "Total overdue invoices",
+          total_payments: "Total payments",
+          view_all: "View all",
+          payment_overview: "Payment overview",
+          invoices_overview: "Invoices overview",
+          income_overview: "Income overview",
+          recent_sold_products: "Recent sold products",
+          recent_quotes: "Recent quotes",
+          recent_invoices: "Recent invoices",
+          recent_payments: "Recent payments",
+          top_clients: "Top clients",
         },
       }
     }
--- config/locales/en/client/dashboards.rb
+++ config/locales/en/client/dashboards.rb
@@ -7,7 +7,14 @@
     client: {
       dashboards: {
         show: {
-          title: "Dashboard"
+          title: "Dashboard",
+          total_quotes: "Total quotes",
+          total_invoices: "Total invoices",
+          total_paid_invoices: "Total paid invoices",
+          total_unpaid_invoices: "Total unpaid invoices",
+          total_payments: "Total payments",
+          top_purchased_products: "Top purchased products",
+          view_all: "View all",
         },
       }
     }
