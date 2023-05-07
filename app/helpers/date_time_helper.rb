69c4d182f74bbbbdb38ef8995b7b38b4d446fd4a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-05-07 18:29:15 +0530

    Added helper methods to show date time with `tooltip`

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -3,6 +3,55 @@
 # -*- warn_indent: true -*-
 
 module DateTimeHelper
+  # Render a `time` element with Javascript-based relative date and tooltip
+  #
+  # time       - Time object
+  # placement  - Tooltip placement String (default: "top")
+  # html_class - Custom class for `time` element (default: "time_ago")
+  #
+  # By default also includes a `script` element with Javascript necessary to
+  # initialize the `timeago` jQuery extension. If this method is called many
+  # times, for example rendering hundreds of commits, it's advisable to disable
+  # this behavior using the `skip_js` argument and re-initializing `timeago`
+  # manually once all of the elements have been rendered.
+  #
+  # A `js-timeago` class is always added to the element, even when a custom
+  # `html_class` argument is provided.
+  #
+  # Returns an HTML-safe String
+  def time_ago_with_tooltip(time, placement: "top", html_class: "", short_format: false)
+    css_classes = [short_format ? "js-short-timeago" : "js-timeago"]
+    css_classes << html_class unless html_class.blank?
+    element = content_tag(:time, time_ago(time),
+                class: css_classes.join(" "),
+                title: time.to_time.in_time_zone,
+                datetime: time.to_time.getutc.iso8601,
+                data: {
+                  controller: "tooltip",
+                  bs_placement: "top",
+                }
+              )
+    element
+  end
+
+  def updated_time_ago_with_tooltip(resource, placement: "top", html_class: "time_ago")
+    content_tag(:span, class: "updated-at-text") do
+      concat(tag.strong do
+        "#{t(".updated")} "
+      end)
+      concat(time_ago_with_tooltip(resource.updated_at, placement: placement, html_class: html_class))
+    end
+  end
+
+  def created_time_ago_with_tooltip(resource, placement: "top", html_class: "time_ago")
+    content_tag(:span, class: "created-at-text") do
+      concat(tag.strong do
+        "#{t(".created")} "
+      end)
+      concat(time_ago_with_tooltip(resource.created_at, placement: placement, html_class: html_class))
+    end
+  end
+
   def time_ago(from_time, to_time = Time.zone.now, options = {})
     options = {
       scope: "datetime.time_ago",
