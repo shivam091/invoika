635be4beda957148e42f8f193c4b9599783734b3
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-06-12 09:59:00 +0530

    Added spec file `UniqueCode` model concern

---
+++ spec/models/concerns/unique_code_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/models/concerns/unique_code_spec.rb
+
+require "spec_helper"
+
+RSpec.describe UniqueCode do
+end
